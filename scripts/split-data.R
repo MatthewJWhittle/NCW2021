682ad9c0cb94ddeb2583148a9b9d35696cdc8ac9
MatthewJWhittle <matthewjwhittle@gmail.com>
2021-08-26 08:23:43 +0100

    R intro

--- scripts/split-data.R
+++ scripts/split-data.R
@@ -4,6 +4,11 @@ require(tidyverse)
 
 data <- read_csv("city_temperature.csv")
 
+data <-
+    data %>% 
+    mutate(AvgTemperature = if_else(AvgTemperature <= -99, true = NA_real_, false = AvgTemperature)) %>% 
+    filter(Year >= 1995)
+
 regions <- unique(data$Region)
 
 file_ids <- regions %>% 
@@ -18,3 +23,14 @@ map2(
     ~data %>% 
     filter(Region == .x) %>% 
     write_csv(glue::glue("data/daily-temperature-{.y}.csv")))
+
+
+# hist(data$AvgTemperature)
+monthly_means <-  
+    data %>% 
+    group_by(Region, Country, State, City, Year, Month) %>% 
+    summarise(monthly_average = mean(AvgTemperature, na.rm = TRUE))
+
+monthly_means %>% write_csv("data/monthly-mean-temps.csv")
+
+data %>% write_csv("city_temperature_tidy.csv")
